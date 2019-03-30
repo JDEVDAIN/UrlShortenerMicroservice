1d53bcac566c983dbd5c814074d1bb99914a6287
JDAIN <poker98@web.de>
2019-03-30 10:20:52 +0100

    urlcontroller returns json now

--- src/main/java/com/website/urlshortenerservice/urlshortener/ShortUrlController.java
+++ src/main/java/com/website/urlshortenerservice/urlshortener/ShortUrlController.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/s")
@@ -19,17 +20,28 @@ public class ShortUrlController {
         return shortUrlService.createShortUrl(shortUrl.getUrl());
     }
 
-    @GetMapping("/{shortUrl}")
-    public void getUrl(@PathVariable String shortUrl, HttpServletResponse resp) throws IOException {
+//    @GetMapping("/{shortUrl}")
+//    public void getUrl(@PathVariable String shortUrl, HttpServletResponse resp) throws IOException {
+//        //TODO make faster with base62 to id conversion and not search for base62 value
+//        String url = shortUrlService.getUrl(shortUrl);
+//        if (url != null) {
+//            resp.addHeader("Location", url);
+//            //TODO CHECK url if valid
+//            resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+//        } else {
+//            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+//        }
+//
+//    }
+
+    @GetMapping(value = "/{shortUrlApi}", produces = "application/json")
+    public HashMap getUrlApi(@PathVariable String shortUrlApi) throws IOException {
         //TODO make faster with base62 to id conversion and not search for base62 value
-        String url = shortUrlService.getUrl(shortUrl);
-        if (url != null) {
-            resp.addHeader("Location", url);
-            //TODO CHECK url if valid
-            resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-        } else {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
+        String url = shortUrlService.getUrl(shortUrlApi);
+        HashMap retHashMap = new HashMap();
+        retHashMap.put("short_url", shortUrlApi);
+        retHashMap.put("url", url);
+        return retHashMap;
 
     }
 }
